74fcf17b24c4ae965be74cbb3f623f6eaf7e3385
salviega <salviega6@gmail.com>
2023-05-29 11:16:23 -0500

    ⚡️ 404 page: Was created 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,34 @@
+import { Box, Button, Text } from '@chakra-ui/react'
+import Image from 'next/image'
+
+import logo from '@/assets/images/brand-transparent.svg'
+import { useRouter } from 'next/router'
+
+export default function NotFoundPage() {
+	const router = useRouter()
+
+	const goHome = () => {
+		router.push('/')
+	}
+
+	return (
+		<Box
+			width={'100%'}
+			height={'100vh'}
+			display={'flex'}
+			flexDir={'column'}
+			alignItems={'center'}
+			justifyContent={'center'}
+			background={'#2b6cb0'}
+		>
+			<Image width={280} src={logo} alt='logo' />
+			<Text marginY={6} fontSize='3xl' as='b'>
+				This Page could not be found
+			</Text>
+			<Text marginBottom={6} fontSize='1xl'>
+				ERRORCODE: 404
+			</Text>
+			<Button onClick={goHome}>GO BACK HOME</Button>
+		</Box>
+	)
+}
